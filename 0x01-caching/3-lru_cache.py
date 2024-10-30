0f4fb61fd4b571c56e2a3e8fc0a0e62702c8d775
SthablyM <mathapelofentse@icloud.com>
2024-10-30 18:50:54 +0000

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-class  LRUCache that inherits from BaseCaching and is a caching system
+LRUCache
 """
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    LRUCache that inherits from BaseCaching
+    """"
+    class LRUCache that inherits from BaseCaching and is a caching system
     """
     def __init__(self):
         """
-        initializing class
+        dictionary from the parent class
         """
         super().__init__()
-        self.order = []
+        self.lru_order = []
 
     def put(self, key, item):
         """
@@ -23,19 +23,20 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
         if key in self.cache_data:
-            self.order.remove(key)
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lru_key = self.order.pop(0)
+            self.lru_order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = self.lru_order.pop(0)
             del self.cache_data[lru_key]
             print(f"DISCARD: {lru_key}")
         self.cache_data[key] = item
-        self.order.append(key)i
+        self.lru_order.append(key)
 
     def get(self, key):
-        """return the value in self.cache_data"""
+        """
+        return the value in self.cache_data linked to key.
+        """
         if key is None or key not in self.cache_data:
             return None
-        self.order.remove(key)
-        self.order.append(key)
-        
+        self.lru_order.remove(key)
+        self.lru_order.append(key)
         return self.cache_data[key]
