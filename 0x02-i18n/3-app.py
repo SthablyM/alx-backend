a7c245e7c110cc985faece51b4c0337325d2cc6c
SthablyM <mathapelofentse@icloud.com>
2024-11-05 22:03:54 +0000

    2-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
-"""task3"""
-from flask import Flask, render_template
-from flask_babel import Babel, gettext as _
+'''Task 2: Get locale from request
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class'''
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
-def home():
-    """default route"""
-    return render_template('3-index.html', title=_('home_title'), header=_('home_header'))
+def home() -> str:
+    '''default route
 
+    Returns:
+        html: homepage
+    '''
+    return render_template("3-index.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
