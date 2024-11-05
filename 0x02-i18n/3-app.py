4fd46c3501c4f643c19d155eab0bcf81b1d29796
SthablyM <mathapelofentse@icloud.com>
2024-11-05 22:00:23 +0000

    2-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+"""task3"""
 from flask import Flask, render_template
-from flask_babel import Babel, _
+from flask_babel import Babel, gettext as _
 
 app = Flask(__name__)
 babel = Babel(app)
 
+
 @app.route('/')
 def home():
-    return render_template('3-index.html')
+    """default route"""
+    return render_template('3-index.html', title=_('home_title'), header=_('home_header'))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run()
-
