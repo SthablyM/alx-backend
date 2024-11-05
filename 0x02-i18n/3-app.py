a9f38d93f78ce80e0ce08e0ce52954c681a28d32
SthablyM <mathapelofentse@icloud.com>
2024-11-05 21:22:36 +0000

    teask 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+@app.route('/')
+def home():
+    return render_template('3-index.html')
+
+if __name__ == "__main__":
+    app.run()
+
